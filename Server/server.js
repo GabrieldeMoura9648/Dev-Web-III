edd96a533e5e64711c1a6ecc3b12d4871ef7fb3c
Gabriel de Moura <gabriel.9648moura@gmail.com>
2026-03-18 10:45:57 -0300

    Server02

--- Server/server.js
+++ Server/server.js
@@ -4,23 +4,50 @@ const path = require('path');
 
 const PORT = 3000;
 
-// Caminho base da pasta Server
 const basePath = __dirname;
+
+function serveHTML(res, fileName) {
+    const filePath = path.join(basePath, fileName);
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            return res.end('Erro ao carregar página');
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+}
 
 const server = http.createServer((req, res) => {
 
-    // 🔒 Garante que só responde GET
     if (req.method !== 'GET') {
         res.writeHead(405, { 'Content-Type': 'text/plain' });
         return res.end('Método não permitido');
     }
 
-    // 📄 PDF
-    if (req.url === '/pdf') {
+    const url = req.url.split('?')[0];
+
+    if (url === '/style.css') {
+        const filePath = path.join(basePath, 'style.css');
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                return res.end('CSS não encontrado');
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(data);
+        });
+    }
+
+    else if (url === '/pdf') {
         const filePath = path.join(basePath, 'exemplo.pdf');
 
-        fs.exists(filePath, (exists) => {
-            if (!exists) {
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+            if (err) {
                 res.writeHead(404);
                 return res.end('Arquivo PDF não encontrado');
             }
@@ -30,12 +57,11 @@ const server = http.createServer((req, res) => {
         });
     }
 
-    // 🖼️ JPEG
-    else if (req.url === '/jpeg') {
+    else if (url === '/jpeg') {
         const filePath = path.join(basePath, 'exemplo.jpeg');
 
-        fs.exists(filePath, (exists) => {
-            if (!exists) {
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+            if (err) {
                 res.writeHead(404);
                 return res.end('Imagem não encontrada');
             }
@@ -45,8 +71,7 @@ const server = http.createServer((req, res) => {
         });
     }
 
-    // 📦 JSON (AGORA LENDO DO ARQUIVO)
-    else if (req.url === '/json') {
+    else if (url === '/json') {
         const filePath = path.join(basePath, 'exemplo.json');
 
         fs.readFile(filePath, 'utf-8', (err, data) => {
@@ -60,24 +85,34 @@ const server = http.createServer((req, res) => {
         });
     }
 
-    // 🌐 HTML
-    else if (req.url === '/html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-
-        res.end(`
-            <html>
-                <head>
-                    <title>Servidor Node</title>
-                </head>
-                <body>
-                    <h1>Servidor funcionando</h1>
-                    <p>Rota HTML ativa.</p>
-                </body>
-            </html>
-        `);
+    else if (url === '/' || url === '/site') {
+        serveHTML(res, 'index.html');
     }
 
-    // ❌ 404
+    else if (url === '/quemsou') {
+        serveHTML(res, 'quemsou.html');
+    }
+
+    else if (url === '/produtos') {
+        serveHTML(res, 'produtos.html');
+    }
+
+    else if (url === '/perguntas') {
+        serveHTML(res, 'perguntas.html');
+    }
+
+    else if (url === '/produto1') {
+        serveHTML(res, 'produto1.html');
+    }
+
+    else if (url === '/produto2') {
+        serveHTML(res, 'produto2.html');
+    }
+
+    else if (url === '/produto3') {
+        serveHTML(res, 'produto3.html');
+    }
+
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('ERROR 404: Rota não encontrada');
